c70059322012d2a64198a459dbf58c0061e5346c
jose <j14castro2001@gmail.com>
2023-11-14 20:53:49 -0300

    añado codigo hibrido

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import sys
+import tp3
 
 AUTO_TEST = "auto"
 EXECUTE = "exec"
@@ -7,6 +8,14 @@ EXECUTE_LINEAL = "exec_lineal"
 AUTO_TEST_GREEDY = "auto_greedy"
 EXECUTE_GREEDY = "exec_greedy"
 CLOSE = "close\n"
+
+def read_data_file(file):
+    subconjuntos = []
+    with open(file) as origen:
+        for linea in origen:
+            nuevo_subconjunto = set().update(linea.split(","))
+            subconjuntos.append(nuevo_subconjunto)
+    return subconjuntos
 
 def auto_test_backtracking(test_files, result_files):
     return
@@ -18,7 +27,9 @@ def auto_test_lineal_greedy(test_files, result_files):
     return
 
 def execute_backtracking(file_name, extra):
-    return
+    subconjuntos = read_data_file(file_name)
+    resultado, k_min = tp3.search_for_min_hitting_set(subconjuntos)
+    return resultado, k_min
 
 def execute_lineal_programming(file_name, extra):
     return
---
+++ tp3.py
@@ -0,0 +1,17 @@
+def search_for_min_hitting_set(subconjuntos):
+
+    return
+
+def _search_for_min_hitting_set(subconjuntos, mejor_sol, sol_act, conjuntos_no_cubiertos):
+
+    if len(conjuntos_no_cubiertos) == 0:
+        return sol_act 
+    
+    if len(mejor_sol) < len(sol_act):
+        return 
+
+    # selecciono algun elemento de a
+
+    _search_for_min_hitting_set(subconjuntos, mejor_sol, sol_act.add(nuevo_elemento), conjuntos_no_cubiertos.elimino_los_que_tengan(nuevo_elemento))
+
+    return
