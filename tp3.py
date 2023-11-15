684c3dedeb70c3f903ea2877feb9e4ff5d54cef0
jose <j14castro2001@gmail.com>
2023-11-15 10:46:53 -0300

    llego pero itera infinitamente

--- tp3.py
+++ tp3.py
@@ -1,5 +1,5 @@
 def search_for_min_hitting_set(subsets, set):
-    return  _search_for_min_hitting_set(set, subsets, [], [])
+    return  _search_for_min_hitting_set(subsets, [], [], 0)
 
 def is_solution(hitting_set, subsets):
     for subset in subsets:
@@ -11,15 +11,33 @@ def is_solution(hitting_set, subsets):
         if not flag: return False 
     return True
 
+def has_a_player(subset, act_sol):
+    for player in subset:
+        if player in act_sol:
+            return True
+    return False
 
-def _search_for_min_hitting_set(set,subsets, best_sol, act_sol): 
+
+def _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub): 
+
+    print(best_sol)
 
     if is_solution(act_sol, subsets) and (len(best_sol) == 0 or len(act_sol) < len(best_sol)):
-        best_sol = act_sol[:]  
-        
+        best_sol = act_sol[:]
+        return best_sol
+    
     if len(best_sol) > 0 and len(act_sol) > len(best_sol) and not is_solution(act_sol, subsets):
-        return
+        return best_sol
 
+    if act_sub > len(subsets) - 1: 
+        return best_sol
 
+    if has_a_player(subsets[act_sub], act_sol):
+        return _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub + 1)
+
+    for player in subsets[act_sub]:
+        act_sol.append(player)
+        best_sol = _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub + 1)
+        act_sol.pop()
 
     return best_sol
