5dbbbdb2240dcc0ac1929cee087f344f8a9b4828
jose <j14castro2001@gmail.com>
2023-11-15 10:23:16 -0300

    elimino el uso de a

--- tp3.py
+++ tp3.py
@@ -14,22 +14,12 @@ def is_solution(hitting_set, subsets):
 
 def _search_for_min_hitting_set(set,subsets, best_sol, act_sol): 
 
-    if len(act_sol) > len(set):
-        return
-
-    if is_solution(act_sol, subsets) and len(best_sol) == 0:
-        best_sol = act_sol[:]
-
-    if len(best_sol) > 0 and is_solution(act_sol, subsets) and len(act_sol) < len(best_sol):
+    if is_solution(act_sol, subsets) and (len(best_sol) == 0 or len(act_sol) < len(best_sol)):
         best_sol = act_sol[:]  
         
     if len(best_sol) > 0 and len(act_sol) > len(best_sol) and not is_solution(act_sol, subsets):
         return
 
-    for element in set:
-        if not element in act_sol: 
-            act_sol.append(element)
-            _search_for_min_hitting_set(set, subsets, best_sol, act_sol)
-            act_sol.pop()
+
 
     return best_sol
