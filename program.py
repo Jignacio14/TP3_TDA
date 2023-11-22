0a19ec19dbb873475c1f006bddf9076883074a4a
jose <j14castro2001@gmail.com>
2023-11-21 21:46:00 -0300

    agrego logica de las pruebas de volumen

--- program.py
+++ program.py
@@ -1,6 +1,12 @@
 import sys
+import os
 import tp3
 
+LECTURA = 'r'
+PREFIX = "resultado_"
+RED = "\033[91m"
+GREEN = "\033[92m"
+RESET = "\033[0m"
 AUTO_TEST = "auto"
 EXECUTE = "exec"
 AUTO_TEST_LINEAL = "auto_lineal"
@@ -21,26 +27,62 @@ def read_data_file(file):
             subconjuntos.append(nuevo_subconjunto)
     return subconjuntos, a
 
-def auto_test_backtracking(test_files, result_files):
+def auto_test_backtracking(test_files_path, result_files_path):
+    result_files_path = result_files_path + "/"
+    test_files_path = test_files_path + "/"
+
+    for file in os.listdir(test_files_path):
+
+        path = os.path.join(test_files_path, file)
+        if not os.path.isfile(path): continue
+
+        subsets, a_set = read_data_file(path)
+        result = function(subsets, a_set)
+        ruta_resultado = test_files_path + PREFIX + path
+        
+        if not os.path.isfile(ruta_resultado): continue
+        
+        #expected_result = read_result(ruta_resultado)
+        expected_result = None
+
+        if(len(result) != expected_result):
+            print(RED, f"Ha ocurrido un error con el algoritmo ! En el archivo: {path}")
+            print(f"El resultado del algoritmo fue: {len(result)} y se esperaba {expected_result}", RESET)
+            break
+        
+        print(f"El obtenido fue conseguido fue: {len(result)}")
+        print("La secuencia de jugadores es: ")
+        print(result)
+        print(GREEN, "TODO OK", RESET, end="\n\n")
+
+def _auto_test_volumn(test_files, result_files, function):
     return
 
-def auto_test_lineal_programming(test_files, result_files):
+def auto_test_lineal_programming(files: list):
+    if not os.path.exists(files[0]) or not os.path.exists(files[1]):
+        print("Alguna de las rutas no es valida")
+        return 
+    _auto_test_volumn(files[0], files[1], tp3.search_hs_linealp)
     return
 
-def auto_test_lineal_greedy(test_files, result_files):
+def auto_test_lineal_greedy(files: list):
+    if not os.path.exists(files[0]) or not os.path.exists(files[1]):
+        print("Alguna de las rutas no es valida")
+        return 
+    _auto_test_volumn(files[0], files[1], tp3.search_for_min_hitting_set)
     return
 
-def execute_backtracking(file_name, extra):
-    subsets, a = read_data_file(file_name)
-    resultado = tp3.search_for_min_hitting_set(subsets, a)
-    return resultado
+def execute_backtracking(file_name: list):
+    subsets, a = read_data_file(file_name[0])
+    print(tp3.search_for_min_hitting_set(subsets, a))
+    return 
 
-def execute_lineal_programming(file_name, extra):
-    subsets, a = read_data_file(file_name)
-    resultado = tp3.search_hs_linealp(subsets, a)
-    return resultado
+def execute_lineal_programming(file_name: list):
+    subsets, a = read_data_file(file_name[0])
+    print(tp3.search_hs_linealp(subsets, a))
+    return
 
-def execute_greedy(file_name, extra):
+def execute_greedy(file_name,):
     return
 
 def show_input_error():
@@ -68,7 +110,13 @@ def program_execution():
         if line == CLOSE:
             print("Adios")
             break
-        # datos = line.strip("\n").split(" ")
-        result = execute_lineal_programming("15.txt", None)
-        print(result)
-        #MAP.get(datos[0], show_input_error())(datos[1])
+        data = line.strip("\n").split(" ")
+        command = data[0] 
+        MAP.get(command, show_input_error)(data[1:])
+
+
+
+    
+
+
+        
