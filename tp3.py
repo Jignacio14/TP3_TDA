544b9f5c804d0782c0ecc09ef0304e894020a136
jose <j14castro2001@gmail.com>
2023-11-23 00:10:58 -0300

    agrego cambios minimos

--- tp3.py
+++ tp3.py
@@ -1,7 +1,7 @@
 import pulp
 
-def search_for_min_hitting_set(subsets, set):
-    return  _search_for_min_hitting_set(subsets, [], [], 0)
+def search_for_min_hitting_set(subsets, a):
+    return  _search_for_min_hitting_set(subsets, [], [], 0, set())
 
 def is_solution(hitting_set, subsets):
     for subset in subsets:
@@ -10,8 +10,7 @@ def is_solution(hitting_set, subsets):
             if element in hitting_set: 
                 flag = True
                 break
-        if not flag: return False 
-    return True
+    return flag
 
 def has_a_player(subset, act_sol):
     for player in subset:
@@ -20,9 +19,10 @@ def has_a_player(subset, act_sol):
     return False
 
 
-def _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub): 
+def _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub, visited): 
 
     if is_solution(act_sol, subsets) and (len(best_sol) == 0 or len(act_sol) < len(best_sol)):
+        visited.add(act_sub)
         best_sol = act_sol[:]
         return best_sol
     
@@ -33,11 +33,11 @@ def _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub):
         return best_sol
 
     if has_a_player(subsets[act_sub], act_sol):
-        return _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub + 1)
+        return _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub + 1, visited)
 
     for player in subsets[act_sub]:
         act_sol.append(player)
-        best_sol = _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub + 1)
+        best_sol = _search_for_min_hitting_set(subsets, best_sol, act_sol, act_sub + 1, visited)
         act_sol.pop()
 
     return best_sol
@@ -55,5 +55,3 @@ def search_hs_linealp(subsets, set):
     
     hitting_set_solution = {var.name for var in dict_variables.values() if pulp.value(var) == 1}
     return hitting_set_solution
-
-
