dfd46e6d8512b4706e426985d4bec6d7d95cc977
jose <j14castro2001@gmail.com>
2023-11-28 00:06:41 -0300

    Opus Magnum

--- program.py
+++ program.py
@@ -27,46 +27,42 @@ def read_data_file(file):
             subconjuntos.append(nuevo_subconjunto)
     return subconjuntos, a
 
-def _auto_test_volumn(test_files_path, result_files_path, function, flag = True):
-    result_files_path = result_files_path + "/"
-    test_files_path = test_files_path + "/"
-
+def _auto_test_volumn(test_files_path, solutions, function):
     for file in os.listdir(test_files_path):
-
-        path = os.path.join(test_files_path, file)
-        if not os.path.isfile(path): continue
-
-        subsets, a_set = read_data_file(path)
-        result = function(subsets, a_set)
-        ruta_resultado = test_files_path + PREFIX + path
-        
-        if not os.path.isfile(ruta_resultado): continue
-        
-        #expected_result = read_result(ruta_resultado)
-        expected_result = None
-
-        if(len(result) != expected_result):
-            print(RED, f"Ha ocurrido un error con el algoritmo ! En el archivo: {path}")
-            print(f"El resultado del algoritmo fue: {len(result)} y se esperaba {expected_result}", RESET)
+        solution = solutions.get(file, None)
+        subsets, a = read_data_file(os.path.join(test_files_path, file))
+        result = function(subsets, a)
+        if(len(result) != solution):
+            print(RED, f"Ha ocurrido un error con el algoritmo ! En el archivo: {file}")
+            print(f"El resultado del algoritmo fue: {len(result)} y se esperaba {solution}", RESET)
             break
-        
+
         print(GREEN, "TODO OK", RESET, end="\n\n")
         print(f"El obtenido fue conseguido fue: {len(result)}")
         print("La secuencia de jugadores es: ")
-        print(result)
+        print(result)     
+    return
+
+def read_solutions(file):
+    solutions = {}
+    with open(file) as origen:
+        for linea in origen:
+            datos = linea.strip("\n").split(":")
+            solutions[datos[0]] = int(datos[1])
+    return solutions
 
 def auto_test_backtracking(files: list):
     if not os.path.exists(files[0]) or not os.path.exists(files[1]):
         print("Some path is not available")
-        return 
-    _auto_test_volumn(files[0], files[1], tp3.search_for_min_hitting_set)
+        return
+    _auto_test_volumn(files[0], read_solutions(files[1]), tp3.search_for_min_hitting_set)
     return
 
 def auto_test_lineal_programming(files: list):
     if not os.path.exists(files[0]) or not os.path.exists(files[1]):
         print("Some path is not available")
         return 
-    _auto_test_volumn(files[0], files[1], tp3.search_hs_linealp)
+    _auto_test_volumn(files[0], read_solutions(files[1]), tp3.search_hs_linealp)
     return
 
 def execute_algorithm(files: list, function):
